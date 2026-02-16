40749db96a27a6e016b9f2a20db32fcba5f63e73
Ilya Petrov <ip4763397@gmail.com>
2026-02-16 13:02:12 +0800

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let tasks = JSON.parse(localStorage.getItem('taskData')) || {taskList: []};
+displayTasks();
+
+function addTask() {
+  const newTask = document.querySelector('.input-bar').value;
+  if (newTask) {
+    tasks.taskList.push(newTask);
+    document.querySelector('.input-bar').value = '';
+    localStorage.setItem('taskData', JSON.stringify(tasks));
+    displayTasks();
+  }
+}
+
+function displayTasks() {
+  let taskString = '';
+  for (let i = 0; i < tasks.taskList.length; i++) {
+    taskString += 
+      `<div class='task-row'>
+        <button class='delete-button' onclick=
+          'deleteTask(${i});'>
+          ${tasks.taskList[i]}
+        </button>
+      </div>`;
+  }
+  document.querySelector('.task-section').innerHTML = taskString;
+}
+
+function deleteTask(i) {
+  tasks.taskList.splice(i, 1);
+  localStorage.setItem('taskData', JSON.stringify(tasks));
+  displayTasks();
+}
