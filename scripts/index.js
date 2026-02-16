dcc63d7164b10e77fe93e07e3de5ab49ece90f9a
Ilya Petrov <ip4763397@gmail.com>
2026-02-16 19:34:25 +0800

    Second Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,17 @@
 let tasks = JSON.parse(localStorage.getItem('taskData')) || {taskList: []};
 displayTasks();
 
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addTask();
+  }
+})
+
+document.body.querySelector('.input-button')
+  .addEventListener('click', () => {
+    addTask();
+  })
+
 function addTask() {
   const newTask = document.querySelector('.input-bar').value;
   if (newTask) {
@@ -16,13 +27,18 @@ function displayTasks() {
   for (let i = 0; i < tasks.taskList.length; i++) {
     taskString += 
       `<div class='task-row'>
-        <button class='delete-button' onclick=
-          'deleteTask(${i});'>
+        <button class='delete-button'>
           ${tasks.taskList[i]}
         </button>
       </div>`;
   }
   document.querySelector('.task-section').innerHTML = taskString;
+  document.querySelectorAll('.delete-button')
+    .forEach((element, index) => {
+      element.addEventListener('click', () => {
+        deleteTask(index);
+      });
+    });
 }
 
 function deleteTask(i) {
